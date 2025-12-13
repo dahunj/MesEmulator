d377c1046a700d40f0b2367589ca8784a203e5f7
DESKTOP-ACFEFB8\USER <dahunj@gmail.com>
2025-12-14 00:47:42 +0900

    에뮬 구현중 1

--- MesEmulator/MesEmulator/Equip.cpp
+++ MesEmulator/MesEmulator/Equip.cpp
@@ -5,11 +5,15 @@
 #include "MesEmulator.h"
 #include "Equip.h"
 #include "LogFile.h"
+#include "MesEmulatorDlg.h"
 
 
 #define	EQUIP_IP	"127.0.0.1"
 #define EQUIP_PORT	11000		// Equip Handler Port
 
+const char STX = 0x02;
+const char ETX = 0x03;
+const CString CRLF = "\r\n";
 
 CEquip g_objEquip;
 // CEquip
@@ -86,19 +90,21 @@ LRESULT CEquip::OnClientReceive(WPARAM wParam, LPARAM lParam)
 	strRecvSocket.Format("%s", byRecv);
 	m_strRecvCmd += strRecvSocket;
 
-	while (!m_strRecvCmd.IsEmpty()) {
-		int nStart = m_strRecvCmd.Find("@");
-		int nEnd = m_strRecvCmd.Find("\n");
+	while (!m_strRecvCmd.IsEmpty()) 
+	{
+		int nStart = m_strRecvCmd.Find(STX);
+		int nEnd = m_strRecvCmd.Find(ETX);
 
 		if (nEnd < 0) break;	// 버퍼에 들어오는 중...
 
-		if (nStart < 0 || nStart > nEnd) {
-			strLog.Format("[<-] : <<Error>> %s : Start(%d), End(%d)", m_strRecvCmd, nStart, nEnd);
+		if (nStart < 0 || nStart > nEnd) 
+		{
+			strLog.Format("[OnClientReceive] <<Error>> - Start(%d), End(%d).\n%s", nStart, nEnd, m_strRecvCmd);
 			g_objLogFile.Save_EquipLog(strLog);
 			m_strRecvCmd.Delete(0, nEnd + 1);	// 쓰레기값이 채워져 있어서...
 			continue;
 		}
-
+		
 		CString strRecv = m_strRecvCmd.Mid(nStart + 1, nEnd - nStart - 1);
 		m_strRecvCmd.Delete(0, nEnd + 1);
 
@@ -107,21 +113,40 @@ LRESULT CEquip::OnClientReceive(WPARAM wParam, LPARAM lParam)
 		g_objLogFile.Save_EquipLog(strLog);
 		/////////////////////////////////////////////////////////////////////////////
 
-		char chSep = ',';
-		CString strCmd, strOp;
+		m_nRecvCmdCount = atoi(strRecv.Mid(8, 4));	// 4Byte
 
-		AfxExtractSubString(strCmd, strRecv, 0, chSep);
-		AfxExtractSubString(strOp, strRecv, 1, chSep);
+		CString strXml = strRecv.Right(strRecv.GetLength() - 13);
+		if (!Extract_Xml(strXml)) return 0;
 
-		CString strArg[10];
-		for (int i = 0; i < 5; i++) AfxExtractSubString(strArg[i], strRecv, i + 2, chSep);
-
-		if (strCmd == "CONTROL") 
-		{
-			//if (strOp == "STATE") Get_ControlState(strArg[0]);
-
-		} 
 	}
 
 	return 0;
+}
+
+
+
+BOOL CEquip::Extract_Xml(CString sXmlData)
+{
+	int k=0;
+	m_strStFn = m_strRcmd = "";	// 초기화
+
+	CMesEmulatorDlg *pMainDlg = (CMesEmulatorDlg*)AfxGetMainWnd();
+	if (!m_xml.LoadXml(sXmlData) ) {
+		CString strLog, strMsg;
+
+		strMsg.Format("[Extract_Xml] CXml Data Load Fail.");
+		//pMainDlg->Set_HostMsg(strMsg);
+
+		strLog.Format("%s\n%s", strMsg, sXmlData);
+		g_objLogFile.Save_EquipLog(strLog);
+
+		return FALSE;
+	}
+
+	CXmlNode node = m_xml.GetRoot();
+	m_strStFn = node.GetAttribute("ID");
+
+	
+	m_xml.Close();
+	return TRUE;
 }
--- MesEmulator/MesEmulator/Equip.h
+++ MesEmulator/MesEmulator/Equip.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "./CXml/Xml.h"
+
+using namespace JWXml;
 // CEquip
 
 class CEquip : public CWnd
@@ -22,7 +24,23 @@ private:
 
 	BOOL	m_bConnected;
 	BOOL	m_bEquipmentOnline;
-	CString m_strRecvCmd;
+
+	CXml	m_xml;
+
+	CString m_strRecvCmd, m_sXMLData;
+
+	int		m_nRecvCmdCount;	// 4Byte
+	int		m_nSendCmdCount;
+
+	CString m_strStFn;	// StreamFunction (S1F1, S2F3, S2F31, S2F49, S6F12)
+	CString m_strRcmd;	// RCMD Command (START, CANCEL, DATA, PERMIT)
+
+	DWORD	m_dwLastTime;	// 마지막 통신 시간
+	CString m_strSetTime;	// Host 설정 시간
+
+	int		m_nS1F4AckNo;	
+	CString m_sHostMsg;
+	CString m_sLastProcID;
 
 public:
 	void Initialize();
@@ -31,6 +49,11 @@ public:
 	BOOL Is_Connected() { return m_bConnected; }
 	BOOL Is_HostOnline() { return m_bEquipmentOnline; }
 	
+	BOOL CEquip::Extract_Xml(CString sXmlData);
+
+private:
+	void Get_S6F11_CarrierIDReport();
+	void Get_S6F11_CarrierOutReport();
 
 };
 
